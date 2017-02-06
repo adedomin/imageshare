eb186423ea739a7ddd2fbb2fd7e213fac6adfdfc
prussian <genunrest@gmail.com>
2017-02-06 01:15:58 -0500

    blah

---
+++ config.default.js
@@ -0,0 +1,32 @@
+var config = {
+    // how many uploads allowed per ms
+    upload_delay_ms: 2000,
+    storage: {
+        max_size: (1024**2)*10,
+        // where to save images
+        dir: '/tmp/uploads',
+        // number of files to retain
+        file_lim: 100
+    },
+    port: 5657,
+    irc: {
+        client: {
+            userName: 'ImageShare',
+            realName: 'bot',
+            port: 6697,
+            autoConnect: true,
+            channels: ['#prussian'],
+            secure: true,
+            floodProtection: true,
+            floodProtectionDelay: 300,
+            sasl: false,
+            stripColors: true,
+            messageSplit: 512,
+        },
+        url: 'https://home.dedominic.pw:5657',
+        server: 'irc.rizon.net',
+        nick: 'ImageIRC-share'
+    }
+}
+
+module.exports = config
---
+++ index.js
@@ -0,0 +1,130 @@
+#!/usr/bin/env node
+/*
+ * Copyright (c) 2017, prussian <genunrest@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+var fs = require('fs'), 
+    path = require('path'),
+    argv = require('yargs').argv,
+    configpath = ''
+
+if (process.env.XDG_CONFIG_HOME)
+    configpath = path.join(process.env.XDG_CONFIG_HOME, 'img-irc.js')
+else if (process.env.HOME)
+    configpath = path.join(process.env.HOME, 'img-irc.js')
+
+
+if (argv._[0] == 'init') {
+    return fs.createReadStream(
+        path.join(__dirname, 'config.default.js')
+    ).pipe(fs.createWriteStream(
+        argv._[1] || configpath
+    ))
+}
+
+var config = require(argv.config || argv.c || configpath),
+    express = require('express'),
+    file_type = require('file-type'),
+    rate_lim = require('express-rate-limit'),
+    Busboy = require('busboy'),
+    read_chunk = require('read-chunk'),
+    files_limit = config.storage.file_lim || 100,
+    IrcClient = require('irc').Client,
+    irc = new IrcClient(config.irc.server, config.irc.nick, config.irc.client)
+    app = express()
+
+if (!config.storage.dir) throw 'You must define a storage directory'
+
+// current latest file
+var curr = (+fs.readdirSync(config.storage.dir).sort((a,b) => +a < +b)[0] + 1)% files_limit || 0
+
+app.enable('trust proxy')
+
+var limit = new rate_lim({
+    windowMs: config.upload_delay_ms || 2000, 
+    max: 1,
+    delayMs: 0
+})
+
+app.use('/upload', limit)
+
+app.post('/upload', (req, res) => {
+    var busboy = new Busboy({ 
+        headers: req.headers,
+        limits: {
+            fileSize: config.storage.max_size || (1024**2)*10,
+            files: 1
+        }
+    })
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        if (mimetype.indexOf('image') != 0) {
+            res.status(500)
+            return res.end({
+                status: 'error',
+                msg: 'not an image'
+            })
+        }
+        file.pipe(fs.createWriteStream(
+            path.join(config.storage.dir, ''+curr)
+        )).on('finish', () => {
+            res.redirect('/uploads/'+curr)
+            config.irc.client.channels.forEach(channel => {
+                irc.say(channel, `new image -> ${config.irc.url}/uploads/${curr}`)
+            })
+            curr = (curr + 1) % files_limit
+        }) 
+    })
+    req.pipe(busboy)
+})
+
+app.get('/uploads/:file', (req, res) => {
+    if (req.params.file.indexOf('/') > -1) {
+        res.status(500)
+        res.end({
+            status: 'error',
+            msg: 'can\'t contain forward slashes (/)'
+        })
+    }
+    res.type(file_type(read_chunk.sync(
+        path.join(config.storage.dir, req.params.file), 0, 4100
+    )).ext)
+    fs.createReadStream(
+        path.join(config.storage.dir, req.params.file)
+    ).pipe(res).on('error', (err) => {
+        res.status(500)
+        res.end({
+            status: 'error',
+            msg: err 
+        })
+    })
+}) 
+
+app.get('/', (req, res) => {
+  res.type('html')
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+    <head><title>upload an image fam</title></head>
+    <body>
+    <form action="/upload" enctype="multipart/form-data" method="post">
+    <input type="file" name="upload"><br>
+    <input type="submit" value="Upload">
+    </form>
+    </body>
+    </html>
+  `)
+})
+
+app.listen(config.port || 5657)
