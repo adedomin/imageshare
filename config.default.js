eb0bfbd31e31869a8299efe9bf0ddb809a459626
Anthony DeDominic <adedomin@gmail.com>
2021-02-14 09:55:32 -0500

    fixup pipelining and handling errors.

--- config.default.js
+++ config.default.js
@@ -5,16 +5,18 @@ const config = {
         window: 10 * 60 * 1000,
         // how many uploads allowed before delays kick in
         delayAfter: 3,
+        // time to delay request (in ms; is multiplied by number of req made in window)
+        delayLength: 1000,
     },
     storage: {
         // 10MiB
         maxSize: (1024*1024)*10,
         // 30kiB
         maxPasteSize: (1024)*30,
-        // where to save images
-        dir: '/tmp/uploads',
-        // where to save pastes
-        pasteDir: '/tmp/pastes',
+        // where to save images (null = random /tmp path)
+        dir: null,
+        // where to save pastes (null = random /tmp path)
+        pasteDir: null,
         // number of files to retain
         fileLimit: 100,
         // number of pastes to retain
